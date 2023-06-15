8e01e4e5c3ba61b067593f750c3e00f454ae9ef7
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-06-14 21:30:32 -0300

    create notes

--- noteapp/src/pages/Home/index.jsx
+++ noteapp/src/pages/Home/index.jsx
@@ -1,6 +1,11 @@
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus, FiSearch } from 'react-icons/fi'
+
 import { Container, Brand, Menu, Search, Content, NewNotes } from './styles'
+
+import { Note } from '../../components/Note'
 import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
@@ -19,11 +24,20 @@ export function Home() {
       </Menu>
 
       <Search>
-
+        <Input placeholder='Pesquisar pelo título' icon={FiSearch} />
       </Search>
 
       <Content>
+        <Section title='minha notas'>
+          <Note data={{
+            title: 'Carlos Felipe',
+            tags: [
+              { id: '1', name: 'React' },
+              { id: '2', name: 'Javascript' }
+            ]
+          }} />
 
+        </Section>
       </Content>
 
       <NewNotes>
