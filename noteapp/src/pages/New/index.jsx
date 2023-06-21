c3c9ee0e6f8e71de53c674eca78f4dc3d27728ad
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-06-20 22:01:46 -0300

    create new notes and text area

---
+++ noteapp/src/pages/New/index.jsx
@@ -0,0 +1,42 @@
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { TextArea } from '../../components/TextArea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+
+import { Container, Form } from './styles'
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder='título' />
+          <TextArea placeholder='Observações' />
+
+          <Section title='Links úteis'>
+            <NoteItem value='https://rocketseat.com.br' />
+            <NoteItem placeholder='Novo link' isNew />
+          </Section>
+
+          <Section title='Marcadores'>
+            <div className='tags'>
+              <NoteItem value='react' />
+              <NoteItem placeholder='Nova tag' isNew />
+            </div>
+          </Section>
+
+          <Button title='salvar'/>
+        </Form>
+      </main>
+    </Container>
+  )
+}
